a798fd9aa292cf761fe9778463c2f6137f15d197
awoestreicher <Awoestreicher@gmail.com>
2022-03-07 02:05:44 -0500

    get by ID

--- routes/studentsRouter.js
+++ routes/studentsRouter.js
@@ -1,72 +1,53 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 const utility = require("../utility")
-const debug = require('debug')('A2-MONGO-CRUD:student')
-const Student = require('../models/Student')
+const debug = require("debug")("A2-MONGO-CRUD:student")
+const Student = require("../models/Student")
 
+router.get("/", async (req, res) => {
+	const students = await Student.find().lean()
+	res.json({
+		data: students.map((student) =>
+			utility.formatResponseData("students", student)
+		),
+	})
+})
 
-router.get('/', async (req, res) => {
-  try {
-    const students = await Student.find().lean()
-    res.json({
-      data: students.map((student) => utility.formatResponseData("students",student))
-    })
+router.post("/", async (req, res) => {
+		let attributes = req.body.data
+		delete attributes._id
+
+		let newStudent = new Student(attributes)
+		await newStudent.save()
+
+		res.status(201).json({
+			data: utility.formatResponseData("students", newStudent),
+		})
+})
+
+router.get("/:id", async (req, res) => {
+	try {
+    const student = await Student.findById(req.params.id).lean()
+    if(!student) throw new error('Resource not found')
+    res.json({data:utility.formatResponseData("students",student)})
 	} catch (err) {
-    debug(`error: ${err}`)
-  }
-
+		utility.sendResourceNotFound(req, res, "student")
+	}
 })
 
-router.post('/', async (req, res) => {
-  try {
-    
-    let attributes = req.body.data
-    delete attributes._id
-
-    let newStudent = new Student(attributes)
-    await newStudent.save()
-
-    res.status(201).json({
-      data: utility.formatResponseData("students", newStudent)
-    })
-
-  } catch(err) {
-
-  }
-
-
+router.patch("/:id", async (req, res) => {
+	try {
+	} catch (err) {}
 })
 
-router.get('/:id', async (req, res) => {
-  try{
-
-  } catch(err) {
-
-  }
+router.put("/:id", async (req, res) => {
+	try {
+	} catch (err) {}
 })
 
-router.patch('/:id', async (req, res) => {
-  try{
-
-  } catch(err) {
-    
-  }
-})
-
-router.put('/:id', async (req, res) => {
-  try{
-
-  } catch(err) {
-    
-  }
-})
-
-router.delete('/:id', async (req, res) => {
-  try{
-
-  } catch(err) {
-    
-  }
+router.delete("/:id", async (req, res) => {
+	try {
+	} catch (err) {}
 })
 
 module.exports = router
