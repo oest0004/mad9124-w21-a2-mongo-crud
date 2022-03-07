4411bdbe2755f4eaf3404d7b1cfe66cd83553010
awoestreicher <Awoestreicher@gmail.com>
2022-03-07 01:54:36 -0500

    POST students logic

--- routes/studentsRouter.js
+++ routes/studentsRouter.js
@@ -1,16 +1,72 @@
 const express = require('express')
 const router = express.Router()
+const utility = require("../utility")
+const debug = require('debug')('A2-MONGO-CRUD:student')
+const Student = require('../models/Student')
 
-router.get('/', async (req, res) => {})
 
-router.post('/', async (req, res) => {})
+router.get('/', async (req, res) => {
+  try {
+    const students = await Student.find().lean()
+    res.json({
+      data: students.map((student) => utility.formatResponseData("students",student))
+    })
+	} catch (err) {
+    debug(`error: ${err}`)
+  }
 
-router.get('/:id', async (req, res) => {})
+})
 
-router.patch('/:id', async (req, res) => {})
+router.post('/', async (req, res) => {
+  try {
+    
+    let attributes = req.body.data
+    delete attributes._id
 
-router.put('/:id', async (req, res) => {})
+    let newStudent = new Student(attributes)
+    await newStudent.save()
 
-router.delete('/:id', async (req, res) => {})
+    res.status(201).json({
+      data: utility.formatResponseData("students", newStudent)
+    })
+
+  } catch(err) {
+
+  }
+
+
+})
+
+router.get('/:id', async (req, res) => {
+  try{
+
+  } catch(err) {
+
+  }
+})
+
+router.patch('/:id', async (req, res) => {
+  try{
+
+  } catch(err) {
+    
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try{
+
+  } catch(err) {
+    
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try{
+
+  } catch(err) {
+    
+  }
+})
 
 module.exports = router
